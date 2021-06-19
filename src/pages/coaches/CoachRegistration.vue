adf57b5495d7a236cb3e4c97a5f34ecdf70c36da
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-19 20:34:07 +0430

    8. Wiring Up Pages

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,3 @@
 <template>
-  REQUESTS
+  REGISTER
 </template>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetails from './pages/coaches/CoachesDetails.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachesRegistration.vue';
-import ContactCoaches from './pages/requests/ContactCoach.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
@@ -15,7 +15,7 @@ const router = createRouter({
     {
       path: '/coaches/:id',
       component: CoachDetails,
-      children: [{ path: '/contact', component: ContactCoaches }]
+      children: [{ path: 'contact', component: ContactCoach }]
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestReceived },
