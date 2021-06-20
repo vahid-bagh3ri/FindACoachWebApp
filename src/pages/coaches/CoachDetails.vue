03a78dded57fbf96d5cb58410c50b01c3272da98
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-20 20:56:40 +0430

    12. Building the Coach Detail Component

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -22,3 +22,36 @@
     </base-card>
   </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.fistName + ' ' + this.selectedCoach.lastName;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    }
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      coach => coach.id === this.id
+    );
+  }
+};
+</script>
