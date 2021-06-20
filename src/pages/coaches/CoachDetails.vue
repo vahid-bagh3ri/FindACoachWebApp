3d76f26b9d1e40ce35e23209214dcd66f4386cd3
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-20 20:36:06 +0430

    12. Building the Coach Detail Component // add CoachDetail's MarkUp

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,24 @@
 <template>
-  DETAILS FOR COACH
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area">
+        <p>{{ description }}</p>
+      </base-badge>
+    </base-card>
+  </section>
 </template>
