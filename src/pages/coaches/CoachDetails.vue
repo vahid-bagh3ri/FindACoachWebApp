ad403cec6a9280a34437dc112b01c907488eb8df
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-21 18:19:06 +0430

    edit firstName in coachDetail

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -37,7 +37,7 @@ export default {
   },
   computed: {
     fullName() {
-      return this.selectedCoach.fistName + ' ' + this.selectedCoach.lastName;
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
     },
     areas() {
       return this.selectedCoach.areas;
