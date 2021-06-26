11b875cf191dcec7b50178bb575f3bed162491ec
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-26 18:08:32 +0430

    22. Fetching Coach Data (GET Http Request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,5 +27,28 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://find-a-coach-web-app-df0e6-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      // ...
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   }
 };
