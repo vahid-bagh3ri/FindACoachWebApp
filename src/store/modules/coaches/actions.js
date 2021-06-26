1ee8b6e90581fcd8ff2ab6c805142460e6f5eb73
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-26 14:34:04 +0430

    21. Sending a PUT Http Request to Store Coach Data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,31 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `https://find-a-coach-web-app-df0e6-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'Put',
+        body: JSON.stringify(coachData)
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
