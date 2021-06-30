f0be06662419d22afc0805fab06c6d1598bfd0eb
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-30 14:08:35 +0430

    25. Sending Coaching Requests Http Requests // sending requests to firebase

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,28 @@
 export default {
-  contactCoach(context, payload) {
-    const newRequests = {
-      id: new Date().toISOString(),
+  async contactCoach(context, payload) {
+    const newRequest = {
       coachId: payload.coachId,
       userEmail: payload.email,
       messsage: payload.message
     };
-    context.commit('addRequest', newRequests);
+    const response = await fetch(
+      `https://find-a-coach-web-app-df0e6-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        methods: 'POST',
+        body: JSON.stringify(newRequest)
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send request.'
+      );
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+
+    context.commit('addRequest', newRequest);
   }
 };
