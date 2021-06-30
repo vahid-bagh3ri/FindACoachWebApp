5a1517c2a603fe1da18e4ad26d0ed42cbd9d38e4
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-30 20:31:19 +0430

    add fetchRequests to action js

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,7 +1,6 @@
 export default {
   async contactCoach(context, payload) {
     const newRequest = {
-      coachId: payload.coachId,
       userEmail: payload.email,
       messsage: payload.message
     };
@@ -22,7 +21,34 @@ export default {
     }
 
     newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
 
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `https://find-a-coach-web-app-df0e6-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch requests!'
+      );
+      throw error;
+    }
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message
+      };
+      requests.push(request);
+    }
+
+    context.commit('setRequests', requests);
   }
 };
