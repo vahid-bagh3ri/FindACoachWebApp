5d9c83eb90b6ec6f9591a7620f29b7635a210d73
-Vhd <vahid-bagheri@outlook.com>
2021-07-04 01:19:55 +0430

    4. Adding an Authentication Page (Login & Signup) // add SubmitForm logic

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,25 +1,57 @@
 <template>
-  <form>
-    <base-card>
+  <base-card>
+    <form @submit.prevent="SubmitForm">
       <div class="form-control">
         <label for="email">E-Mail</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" v-model.trim="email" />
       </div>
       <div class="form-control">
         <label for="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" v-model.trim="password" />
       </div>
+      <p v-if="!formIsValid">
+        Please enter a valid email and password (must be at least 6 characters
+        long)
+      </p>
       <base-button>Login</base-button>
-      <base-button type="button" mode="flat">Singup instead</base-button>
-    </base-card>
-  </form>
+      <base-button type="button" mode="flat" @click="switchAuthMode"
+        >Singup instead</base-button
+      >
+    </form>
+  </base-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      passwrod: '',
+      formIsValid: true,
+      mode: 'login '
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      // sned http request...
+    },
+    switchAuthMode() {}
+  }
+};
+</script>
 
 <style scoped>
 form {
   margin: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 12px;
   padding: 1rem;
 }
 
