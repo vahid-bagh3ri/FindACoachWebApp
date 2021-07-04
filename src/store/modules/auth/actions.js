b19d0609cbadd8c86c9515b96dcdf1d1b470ef42
-Vhd <vahid-bagheri@outlook.com>
2021-07-04 23:12:49 +0430

    13. Adding Auto Login //add auth action and make code more cleaner

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,48 +1,37 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAs8V4lECWOTcPI266qnjXb6YhavldvHQw',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
-      throw error;
-    }
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAs8V4lECWOTcPI266qnjXb6YhavldvHQw',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'singup'
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAs8V4lECWOTcPI266qnjXb6YhavldvHQw';
+
+    if (mode === 'singup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAs8V4lECWOTcPI266qnjXb6YhavldvHQw';
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.message || 'Failed to authenticate. Check your login data.'
       );
