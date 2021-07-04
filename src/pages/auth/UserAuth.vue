ae46b34bef91113ee4a5aea019911c4d224ac638
-Vhd <vahid-bagheri@outlook.com>
2021-07-04 19:36:28 +0430

    8. Adding a Login Action & Flow

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -71,15 +71,17 @@ export default {
 
       this.isLoading = true;
 
+      const actionPayload = {
+        email: this.email,
+        password: this.password
+      };
+
       try {
         if (this.mode === 'login') {
           console.log('login');
-          // ..
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
       } catch (err) {
         this.error = err.message || 'Failed to authenticate, try later.';
