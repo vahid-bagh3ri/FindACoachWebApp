99c3f270830d060057e331dc89849d130b812989
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-07-04 15:04:17 +0430

    6. Adding a Signup Action & Flow // add singup method to ./auth/actions.js

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,26 @@
-export default {};
+export default {
+  login() {},
+  async singup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAs8V4lECWOTcPI266qnjXb6YhavldvHQw',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+    console.log(responseData);
+  }
+};
