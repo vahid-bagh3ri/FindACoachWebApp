61fefe985b438d30e6998cc5958b1e196b98e01c
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-07-05 12:36:29 +0430

    13. Adding Auto Login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -37,13 +37,30 @@ export default {
       );
       throw error;
     }
-    console.log(responseData);
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     });
   },
+
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      });
+    }
+  },
+
   logout(context) {
     context.commit('setUser', {
       toket: null,
